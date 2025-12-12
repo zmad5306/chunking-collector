6295a46e2da74c5d2c0a75c6d3a6ea60a6572130
zmad5306 <zmad5306@users.noreply.github.com>
2025-12-12 03:42:43 +0000

    deploy: 056e11effe8b6406ec810b22c6ce808a5735e868

---
+++ 1.2.0/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"dev.zachmaddox.chunking","l":"Chunking"},{"p":"dev.zachmaddox.chunking","l":"Chunking.RemainderPolicy"}];updateSearchResults();
